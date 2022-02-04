e11dbe85ffcfa846194388a4fafe867259120262
Jordan <jrdns@DESKTOP-0UQTEKN>
2022-02-04 13:24:15 -0500

    added class Restaurant

---
+++ Reserver/Models/Restaurant.cs
@@ -0,0 +1,13 @@
+﻿namespace Reserver.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description   { get; set; }
+
+        public int Capacity { get; set; }
+    }
+}
